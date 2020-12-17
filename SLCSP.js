a83dfbbc17b9057d9fe81c110514aff0482b0afd
zlm <zachmurray.dev@gmail.com>
2020-12-16 21:14:00 -0600

    SLCSP_Array0

--- SLCSP.js
+++ SLCSP.js
@@ -1,20 +1,28 @@
 const fs = require('fs');
 const parse = require('csv-parse');
 
+let csvData;
 const csvPromise = ((csvFilepath) => {
   return new Promise((resolve) => {
-    let csvData = [];
+    csvData = [];
     fs.createReadStream(csvFilepath)
       .pipe(parse({ delimiter: ',' }))
       .on('data', (csvRow) => { csvData.push(csvRow); })
       .on('end', () => { resolve(csvData); });
-  })
+  }).catch((error) => { console.log(error) });
 })
 
-let SLCSP_Data = csvPromise('slcsp.csv').then((value) => { return value; });
-let Plans_Data = csvPromise('slcsp.csv').then((value) => { return value; });
-let ZIPs_Data = csvPromise('slcsp.csv').then((value) => { return value; });
+let SLCSP_Array, Plans_Array, Zips_Array;
+let SLCSP_Promise = csvPromise('slcsp.csv').then((value) => { SLCSP_Array = value; });
+let Plans_Promise = csvPromise('plans.csv').then((value) => { Plans_Array = value; });
+let Zips_Promise = csvPromise('zips.csv').then((value) => { Zips_Array = value; });
 
-SLCSP_Data.then((value) => { console.log(`\nSLCSP_Data:\n`, value) });
-Plans_Data.then((value) => { console.log(`\nPlans_Data:\n`, value) });
-ZIPs_Data.then((value) => { console.log(`\nZIPs_Data:\n`, value) });
+SLCSP_Promise.then(() => { for (let i = 0; i < 5; i++) { console.log(`SLCSP_Array[${i}]`, SLCSP_Array[i]); } });
+Plans_Promise.then(() => { for (let i = 0; i < 5; i++) { console.log(`Plans_Array[${i}]`, Plans_Array[i]); } });
+Zips_Promise.then(() => { for (let i = 0; i < 5; i++) { console.log(`Zips_Array[${i}]`, Zips_Array[i]); } });
+
+let SLCSP_Array0;
+SLCSP_Promise.then(() => {
+  SLCSP_Array0 = [...new Set(SLCSP_Array.map((x) => { return x[0] }))]
+  for (let i = 0; i < 5; i++) { console.log(`SLCSP_Array[${i}]`, SLCSP_Array0[i]) }
+})
