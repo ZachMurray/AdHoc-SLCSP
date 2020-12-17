8cd8f4b4ef77fdb24e688e4b82b94604c3487fa6
zlm <zachmurray.dev@gmail.com>
2020-12-16 20:04:53 -0600

    autoformat

--- SLCSP.js
+++ SLCSP.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const parse = require('csv-parse');
 
-const csvPromise = (csvFilepath) => {
+const csvPromise = ((csvFilepath) => {
   return new Promise((resolve) => {
     let csvData = [];
     fs.createReadStream(csvFilepath)
@@ -9,12 +9,12 @@ const csvPromise = (csvFilepath) => {
       .on('data', (csvRow) => { csvData.push(csvRow); })
       .on('end', () => { resolve(csvData); });
   })
-}
+})
 
 let SLCSP_Data = csvPromise('slcsp.csv').then((value) => { return value; });
 let Plans_Data = csvPromise('slcsp.csv').then((value) => { return value; });
 let ZIPs_Data = csvPromise('slcsp.csv').then((value) => { return value; });
 
-SLCSP_Data.then((value)=>{console.log(`\nSLCSP_Data:\n`, value)});
-Plans_Data.then((value)=>{console.log(`\nPlans_Data:\n`, value)});
-ZIPs_Data.then((value)=>{console.log(`\nZIPs_Data:\n`, value)});
+SLCSP_Data.then((value) => { console.log(`\nSLCSP_Data:\n`, value) });
+Plans_Data.then((value) => { console.log(`\nPlans_Data:\n`, value) });
+ZIPs_Data.then((value) => { console.log(`\nZIPs_Data:\n`, value) });
