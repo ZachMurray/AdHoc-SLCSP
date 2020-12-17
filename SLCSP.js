76f3224bb48a9a19a7c76f1603a278e4c6ed01f8
zlm <zachmurray.dev@gmail.com>
2020-12-16 18:51:28 -0600

    wait for promise

--- SLCSP.js
+++ SLCSP.js
@@ -1,15 +1,6 @@
 const fs = require('fs');
 const parse = require('csv-parse');
 
-// let csvFilepath = 'slcsp.csv';
-// const csvPromise = new Promise((resolve, reject) => {
-//   let csvData = [];
-//   fs.createReadStream(csvFilepath)
-//     .pipe(parse({ delimiter: ',' }))
-//     .on('data', (csvRow) => { csvData.push(csvRow); })
-//     .on('end', () => { resolve(csvData); });
-// });
-
 const csvPromise = (csvFilepath) => {
   return new Promise((resolve) => {
     let csvData = [];
@@ -20,7 +11,10 @@ const csvPromise = (csvFilepath) => {
   })
 }
 
-csvPromise.then((value) => {
-  console.log(value);
-});
+let SLCSP_Data = csvPromise('slcsp.csv').then((value) => { return value; });
+let Plans_Data = csvPromise('slcsp.csv').then((value) => { return value; });
+let ZIPs_Data = csvPromise('slcsp.csv').then((value) => { return value; });
 
+SLCSP_Data.then((value)=>{console.log(`\nSLCSP_Data:\n`, value)});
+Plans_Data.then((value)=>{console.log(`\nPlans_Data:\n`, value)});
+ZIPs_Data.then((value)=>{console.log(`\nZIPs_Data:\n`, value)});
