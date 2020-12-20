5bd5ce8c84c875441333bab0e518c77fca5ee9c5
zlm <zachmurray.dev@gmail.com>
2020-12-19 22:21:53 -0600

    can't continue with maps because duplicate zipcode

--- app_slcsp2.js
+++ app_slcsp2.js
@@ -11,20 +11,24 @@ const Csv2Array_Promise = (async (csvFilepath) => {
   });
 })
 
-const Csv2Map_Promise = (async (csvFilepath) => {
+const Csv2Map_Promise = (async (csvFilepath_String, Properties_Array) => {
   return new Promise((resolve) => {
     const csvData = new Map();
-    createReadStream(csvFilepath)
+    createReadStream(csvFilepath_String)
       .pipe(parse({ delimiter: ',' }))
-      .on('data', (csvRow) => { csvData.set(csvRow[0], { 'state': 1, 'county_code': 2, 'name': 3, 'rate_area': 4 }); })
+      .on('data', (csvRow) => {
+        csvData.set(csvRow[0], { Properties_Array[1]: csvRow[1], 'county_code': 2, 'name': 3, 'rate_area': 4 });
+      })
       .on('end', () => { resolve(csvData); });
   });
 })
 
 let Slcsp_Array, Zips_Map, Plans_Map;
 Csv2Array_Promise('slcsp.csv').then((value) => { Slcsp_Array = value; });
-const ZipsMap_Promise = Csv2Map_Promise('zips.csv').then((value) => { Zips_Map = value; });
-Csv2Map_Promise('plans.csv').then((value) => { Plans_Map = value; });
+const ZipsMap_Promise = Csv2Map_Promise('zips.csv', ['zipcode', 'state', 'county_code', 'name', 'rate_area'])
+  .then((value) => { Zips_Map = value; });
+Csv2Map_Promise('plans.csv', ['plan_id', 'state', 'metal_level', 'rate', 'rate_area'])
+  .then((value) => { Plans_Map = value; });
 
 ZipsMap_Promise.then(() => {
   let limit = 10;
