636a9d04af11905843664efa16adb274320a071d
zlm <zachmurray.dev@gmail.com>
2020-12-22 22:41:30 -0600

    Try-4

--- app_slcsp4.js
+++ app_slcsp4.js
@@ -1,48 +1,31 @@
 import { createReadStream } from 'fs';
 import parse from 'csv-parse';
 
-let SlcspZips_Rates_Array, ZipCode_RateArea_Map, RateAreaSilvers_Rates_Map;
+let SlcspCsv_Array, ZipsCsv_Map, PlansCsv_Map;
 
-const Csv2Array_Promise = (csvFilepath => new Promise(resolve => {
-  const csvData = [];
+const SlcspCsv_Array_Promise = (csvFilepath => new Promise(resolve => {
+  const csvData = []
   createReadStream(csvFilepath)
     .pipe(parse({ delimiter: ',' }))
-    .on('data', (csvRow) => { csvData.push(csvRow); })
-    .on('end', () => { resolve(csvData); });
+    .on('data', csvRow => csvData.push(csvRow))
+    .on('end', () => { resolve(csvData) })
 }))
 
-const ZipCode_RateArea_Map_Promise = (csvFilepath => new Promise(resolve => {
-  const ZipCode_RateArea_Map = new Map();
+const Csv_Map_Promise = (csvFilepath => new Promise(resolve => {
+  const csvData = new Map()
   createReadStream(csvFilepath)
     .pipe(parse({ delimiter: ',' }))
-    .on('data', (csvRow) => {
-      if (ZipCode_RateArea_Map.has(csvRow[0]))
-        ZipCode_RateArea_Map.set(csvRow[0], [...ZipCode_RateArea_Map.get(csvRow[0], csvRow[4])])
-      else
-        ZipCode_RateArea_Map.set(csvRow[0], [csvRow[4]])
-    })
-    .on('end', () => { resolve(ZipCode_RateArea_Map); });
-}));
-
-const RateAreaSilvers_Rates_Promise = (csvFilepath => new Promise(resolve => {
-  const RateAreaSilvers_Rates_Map = new Map();
-  createReadStream(csvFilepath)
-    .pipe(parse({ delimiter: ',' }))
-    .on('data', (csvRow) => {
-      if (csvRow[2] != 'Silver')
-        return;
-      if (RateAreaSilvers_Rates_Map.has(csvRow[4]))
-        RateAreaSilvers_Rates_Map.set(csvRow[4], [...RateAreaSilvers_Rates_Map.get(csvRow[4]), csvRow[3]]);
-
-      else
-        RateAreaSilvers_Rates_Map.set(csvRow[4], [csvRow[3]]);
-    }).on('end', () => { resolve(RateAreaSilvers_Rates_Map); });
-}));
+    .on('data', csvRow => csvData.set(csvRow[0], csvRow.slice(1)))
+    .on('end', () => resolve(csvData))
+}))
 
 Promise.all([
-  Csv2Array_Promise('slcsp.csv').then(value => { SlcspZips_Rates_Array = value }),
-  ZipCode_RateArea_Map_Promise('zips.csv').then(value => { ZipCode_RateArea_Map = value }),
-  RateAreaSilvers_Rates_Promise('plans.csv').then(value => { RateAreaSilvers_Rates_Map = value })
-]).then(() => {
-  
-})
+  SlcspCsv_Array_Promise('slcsp.csv').then(value => SlcspCsv_Array = value),
+  Csv_Map_Promise('zips.csv').then(value => ZipsCsv_Map = value),
+  Csv_Map_Promise('plans.csv').then(value => PlansCsv_Map = value)
+])
+  .then(() => {
+    // console.log(SlcspCsv_Array)
+    // console.log(ZipsCsv_Map)
+    console.log(PlansCsv_Map)
+  })
