5f3755ecf2187c537e736ffc997326afd6b0e1cb
zlm <zachmurray.dev@gmail.com>
2020-12-29 13:42:52 -0600

    Cleanup

--- app_slcsp5.js
+++ app_slcsp5.js
@@ -23,29 +23,20 @@ Promise.all([
     if (!SlcspZips_ZipsRateAreas_Map.has(Zip_Entry[0])) continue;
     SlcspZips_ZipsRateAreas_Map.set(
       Zip_Entry[0],
-      [
-        ...SlcspZips_ZipsRateAreas_Map.get(Zip_Entry[0]),
-        Zip_Entry[4]
-      ]
+      [...SlcspZips_ZipsRateAreas_Map.get(Zip_Entry[0]), Zip_Entry[4]]
     )
   }
   const SlcspZips_Plans_Map = new Map();
   SlcspZips_ZipsRateAreas_Map.forEach((RateAreas, Zip) => {
-    // console.log('RateAreas', RateAreas)
     const Plans = SlcspZips_Plans_Map.get(Zip) ?? new Array()
-    // console.log('Plans', Plans)
     for (let Plan_Entry of Plans_Array) {
       if (Plan_Entry[2] != 'Silver' || !RateAreas.includes(Plan_Entry[4])) continue;
       Plans.push(Plan_Entry)
     }
     SlcspZips_Plans_Map.set(Zip, [...new Set(Plans)]);
   })
-  // console.log(SlcspZips_Plans_Map);
   const SlcspZips_Slcsp_Array = [];
   SlcspZips_Plans_Map.forEach((Plans, Zip) => {
-    // console.log(Plans);
-    // const Plans_Rate_Array = Plans.map(Plan => { return Plan[3] })
-    // SlcspZips_Slcsp_Array.push([Zip, Plans_Rate_Array[2]])
     SlcspZips_Slcsp_Array.push([Zip, Plans.map(Plan => { return Plan[3] })[2]])
   })
   SlcspZips_Slcsp_Array[0] = SLCSP_Array[0]
